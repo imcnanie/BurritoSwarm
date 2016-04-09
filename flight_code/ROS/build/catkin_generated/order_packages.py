d338594fdf954b3bd0585c1dbafa86081647c03a
Ian McNanie <ian.mcnanie@gmail.com>
2016-04-08 21:54:18 -0700

    added ROS package, moved com.py to dronekit/

---
+++ flight_code/ROS/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ian/BurritoSwarm/flight_code/ROS/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ian/3DR_Projects/dronekit-mavros/devel;/opt/ros/jade".split(';') if "/home/ian/3DR_Projects/dronekit-mavros/devel;/opt/ros/jade" != "" else []
---
+++ flight_code/ROS/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ian/BurritoSwarm/flight_code/ROS/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/ian/BurritoSwarm/flight_code/ROS/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/ian/BurritoSwarm/flight_code/ROS/devel/lib:/home/ian/BurritoSwarm/flight_code/ROS/devel/lib/x86_64-linux-gnu:/home/ian/3DR_Projects/dronekit-mavros/devel/lib/x86_64-linux-gnu:/opt/ros/jade/lib/x86_64-linux-gnu:/home/ian/3DR_Projects/dronekit-mavros/devel/lib:/home/ian/solo_ws/devel_isolated/mavlink/lib:/home/ian/solo_ws/devel_isolated/mavlink/lib/x86_64-linux-gnu:/opt/ros/jade/lib"
+export PATH="/home/ian/BurritoSwarm/flight_code/ROS/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/ian/BurritoSwarm/flight_code/ROS/devel/lib/pkgconfig:/home/ian/BurritoSwarm/flight_code/ROS/devel/lib/x86_64-linux-gnu/pkgconfig:/home/ian/3DR_Projects/dronekit-mavros/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/jade/lib/x86_64-linux-gnu/pkgconfig:/home/ian/3DR_Projects/dronekit-mavros/devel/lib/pkgconfig:/home/ian/solo_ws/devel_isolated/mavlink/lib/pkgconfig:/home/ian/solo_ws/devel_isolated/mavlink/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/jade/lib/pkgconfig"
+export PWD="/home/ian/BurritoSwarm/flight_code/ROS/build"
+export PYTHONPATH="/home/ian/BurritoSwarm/flight_code/ROS/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROS_PACKAGE_PATH="/home/ian/BurritoSwarm/flight_code/ROS/src:$ROS_PACKAGE_PATH"
