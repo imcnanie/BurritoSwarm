078d1aa27afc10834e44084f986db3de90efb0b0
Ian McNanie <ianmcnanie@gmail.com>
2016-04-09 23:13:09 -0700

    Cleaned up velocity_goto.py

--- flight_code/ROS/src/burrito/nodes/velocity_goto.py
+++ flight_code/ROS/src/burrito/nodes/velocity_goto.py
@@ -36,13 +36,8 @@ class brekinIt:
         self.throttle_update = 0.0
         self.attitude_publish = True
 
-
         self.pid_throttle = pid_controller.PID(P = 0.8, I = 0.8, D = 0.8, Integrator_max=1, Integrator_min=0)
         self.pid_throttle.setPoint(10)
-
-
-       
-        
 
         self.setHome = False
         self.home_lat = 0.0
@@ -51,26 +46,16 @@ class brekinIt:
         self.publish = True
         self.velocity_init()
 
-        #self.velocity_init()
-
-
-
     def setmode(self,base_mode=0,custom_mode="OFFBOARD",delay=2):
         # Optimize time delay
         set_mode = rospy.ServiceProxy('/mavros/set_mode', mavros_msgs.srv.SetMode)  
         ret = set_mode(base_mode=base_mode, custom_mode=custom_mode)
         print "Changing modes: ", ret
         time.sleep(delay)
-
-
-
-        
+    
     def handle_pose(self, msg):
         pos = msg.pose.pose.position
-        #q = msg.pose.pose.orientation
-        #msg.pose.pose.position.x = 465720.756519
-        #pub.publish(msg)
-    
+        
         self.current_lat = pos.x 
         self.current_lon = pos.y
         self.current_alt = pos.z
@@ -82,15 +67,6 @@ class brekinIt:
             self.home_alt = self.current_alt
             self.setHome = True
         
-        #print "X ",q.x, " Y ", q.y, " Z ", q.z, " W ", q.w
-    
-        #br = tf.TransformBroadcaster()
-        #br.sendTransform(( pos.x - 465710.758519, pos.y - 5249465.35307,  pos.z),
-        #                 (q.x, q.y, q.z, q.w),
-        #                 rospy.Time.now(),
-        #                 "f1",
-        #                 "fcu")
-
     def reset_home(self):
         self.setHome = False
         
@@ -107,126 +83,6 @@ class brekinIt:
         s = Thread(target=self.subscribe_pose, args=())
         s.daemon = True
         s.start()
-
-    def gps_goto(self, lat, lon, alt, delay=0):
-        #print "in coord"
-        self.x = lat - self.home_lat
-        self.y = lon - self.home_lon
-        self.z = alt
-        time.sleep(delay)
-        
-    def set_coord(self, lat, lon, alt, delay=0):
-        #print "in coord"
-        self.x = lat - self.home_lat
-        self.y = lon - self.home_lon
-        self.z = alt
-        time.sleep(delay)
-
-    def set_local_coord(self, lat, lon, alt, delay=0):
-        #print "in coord"
-        self.x = lat
-        self.y = lon
-        self.z = alt
-        time.sleep(delay)
-
-
-    def set_publish(self, pub):
-        self.publish = pub
-        
-    def publish_pose(self):
-        self.pub = SP.get_pub_position_local(queue_size=10)
-        rate = rospy.Rate(10)   # 10hz
-        
-        msg = SP.PoseStamped(
-            header=SP.Header(
-                frame_id="base_footprint",  # no matter, plugin don't use TF
-                stamp=rospy.Time.now()),    # stamp should update
-        )
-        
-        while not rospy.is_shutdown():
-            #msg.pose.position.x = self.x
-            #msg.pose.position.y = self.y
-            #msg.pose.position.z = self.z
-            msg.pose.position.x = 0.0
-            msg.pose.position.y = 0.0
-            msg.pose.position.z = 0.0
-            
-            
-            # For demo purposes we will lock yaw/heading to north.
-            yaw_degrees = self.yaw  # North
-            yaw = radians(yaw_degrees)
-            quaternion = quaternion_from_euler(0, 0, yaw)
-            msg.pose.orientation = SP.Quaternion(*quaternion)
-            if self.publish == True:
-                self.pub.publish(msg)
-                print "publishing position"
-            rate.sleep()
-
-    def publish_angular_linear_orientation(self):
-
-        #att_pub = SP.get_pub_attitude_pose(queue_size=10)
-        #thd_pub = SP.get_pub_attitude_throttle(queue_size=10)
-        vel_pub = SP.get_pub_velocity_cmd_vel(queue_size=10)
-        
-        rate = rospy.Rate(10)   # 10hz
-        
-        ## msg = SP.PoseStamped(
-        ##     header=SP.Header(
-        ##         frame_id="base_footprint",  # no matter, plugin don't use TF
-        ##         stamp=rospy.Time.now()),    # stamp should update
-        ## )
-        
-        while not rospy.is_shutdown():
-            #msg.pose.position.x = self.x
-            #msg.pose.position.y = self.y
-            #msg.pose.position.z = self.z
-            twist = SP.TwistStamped(header=SP.Header(stamp=rospy.get_rostime()))
-            twist.twist.linear = SP.Vector3(x=0, y=0, z=0)
-            twist.twist.angular = SP.Vector3(z=-0.4)
-            print "publishing orientation"
-            vel_pub.publish(twist)
-
-    def publish_orientation(self, ang=180):
-
-        
-        
-        att_pub = SP.get_pub_attitude_pose(queue_size=10)
-        
-        #thd_pub = SP.get_pub_attitude_throttle(queue_size=10)
-        #vel_pub = SP.get_pub_velocity_cmd_vel(queue_size=10)
-        
-        rate = rospy.Rate(10)   # 10hz
-        
-        ## msg = SP.PoseStamped(
-        ##     header=SP.Header(
-        ##         frame_id="base_footprint",  # no matter, plugin don't use TF
-        ##         stamp=rospy.Time.now()),    # stamp should update
-        ## )
-        
-        while not rospy.is_shutdown():
-            #msg.pose.position.x = self.x
-            #msg.pose.position.y = self.y
-            #msg.pose.position.z = self.z
-
-
-            pose = SP.PoseStamped(header=SP.Header(stamp=rospy.get_rostime()))
-            q = quaternion_from_euler(0, 0, ang)
-            pose.pose.orientation = SP.Quaternion(*q)
-            
-            #thd_pub.publish(data=self.throttle_update)
-            #thd_pub.publish(data=0.6)
-            
-            att_pub.publish(pose)
-            
-            print "pose orientation: ", self.throttle_update
-            
-            
-    def publish_pose_thread(self):
-        #t = Thread(target=self.publish_orientation, args=())
-        t = Thread(target=self.publish_pose, args=())
-        t.daemon = True
-        t.start()
-
         
     def arm(self):
         print dir(mavros)
@@ -236,42 +92,6 @@ class brekinIt:
     def disarm(self):
         arm = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)  
         print "Disarm: ", arm(False)
-
-    def land(self):
-        # Make margin hella better
-
-        lat = self.current_lat
-        lon = self.current_lon
-
-        #ret = command.takeoff(altitude=alt)
-        land = rospy.ServiceProxy('/mavros/cmd/land', mavros_msgs.srv.CommandTOL)
-        land(0, 0, lat, lon, 0)  
-        #while abs(self.current_alt - alt) > 0.5:
-        #    self.set_coord(lat, lon, alt, delay=0.1)
-        #    print "going to alt", self.current_alt - alt
-
-
-        time.sleep(2)
-        
-        rospy.loginfo("Reached target Alt!")
-        
-    ##def land(self):
-    ##    # Simulate a slow landing.
-    ##    lat = self.current_lat
-    ##    lon = self.current_lon
-    ##    
-    ##    self.set_coord(lat, lon,  8.0, delay=5)
-    ##    self.set_coord(lat, lon,  3.0, delay=5)
-    ##    self.set_coord(lat, lon,  2.0, delay=2)
-    ##    self.set_coord(lat, lon,  1.0, delay=2)
-    ##    self.set_coord(lat, lon,  0.0, delay=2)
-    ##    self.set_coord(lat, lon, -0.2, delay=2)
-
-    ##    rospy.loginfo("Landed?")
-        
-
-
-
 
     
     """
@@ -302,21 +122,6 @@ class brekinIt:
         self.done_evt = threading.Event()
  
     def navigate(self):
-
-
-
-        #att_pub = SP.get_pub_attitude_pose(queue_size=10)
-        #thd_pub = SP.get_pub_attitude_throttle(queue_size=10)
-            
-            
-            #thd_pub.publish(data=self.throttle_update)
-            #thd_pub.publish(data=0.6)
-            
-            
-            
-
-
-        
         rate = rospy.Rate(10)   # 10hz
         magnitude = 1  # in meters/sec
 
@@ -334,17 +139,13 @@ class brekinIt:
                 msg.twist.linear = geometry_msgs.msg.Vector3(self.vx*magnitude, self.vy*magnitude, self.vz*magnitude+pid_offset)
             else:
                 msg.twist.linear = geometry_msgs.msg.Vector3(self.vx*magnitude, self.vy*magnitude, self.vz*magnitude)
-            #print msg.twist.linear
-            # For demo purposes we will lock yaw/heading to north.
+
+            # Yaw won't work
             yaw_degrees = self.yaw  # North
             yaw = radians(yaw_degrees)
             quaternion = quaternion_from_euler(0, 0, yaw)
             if self.velocity_publish:
-                #msg.twist.angular.z = 0.5
                 self.pub_vel.publish(msg)
-                #print "PID: ", pid_offset
-
-
             
             rate.sleep()
             i +=1
@@ -377,7 +178,6 @@ class brekinIt:
         self.use_pid = True
         pid_lat.setPoint(lat)
 
-        self.set_publish(False)
         self.set_velocity_publish(True)
         
         while abs(self.current_lat - lat) > 0.2:
@@ -392,7 +192,7 @@ class brekinIt:
             print "ABS current lat min lat: ", abs(self.current_lat - lat)
             #print "Current lat, target lat: ", self.current_lat, " ", lat
 
-        self.set_publish(False)
+
             
     def land_velocity(self):
         self.use_pid = False
@@ -407,18 +207,14 @@ class brekinIt:
     def takeoff_velocity(self, alt=7):
         # Make margin hella better
         
-        #ret = command.takeoff(altitude=alt)
-        #takeoff = rospy.ServiceProxy('/mavros/cmd/takeoff', mavros_msgs.srv.CommandTOL)
-        #takeoff( 0, 0, lat, lon, 1)
         self.use_pid = False
         while abs(self.current_alt - alt) > 0.2:
-            #self.set_local_coord(0, 0, alt, delay=0.1)
+        
             self.x = 0
             self.y = 0
             self.z = 10
             self.set_velocity(0, 0, 2.5)
-            #print "ye ",abs(self.current_alt - alt)
-            #print "going to alt", self.current_alt,  alt
+        
 
         time.sleep(2)
         
@@ -463,25 +259,18 @@ if __name__ == '__main__':
     print "out of takeoff"
 
 
-    brekin.set_publish(False)
-
-
     brekin.blocked_yaw()
     print "blocked_yaw"
     time.sleep(1)
-    #brekin.set_velocity_publish(True)
-    #print "setting velocity"
-    #brekin.set_velocity(-1, 0, 0.2)
+
     print "going to gps"
     brekin.velocity_gps_goto(465700.071181, brekin.current_lon, 10)
     print "at gps, waiting"
     time.sleep(1)
     print "done"
-    #brekin.set_velocity(0, 0, 0.2, yaw=180)
-    #time.sleep(5)
-    #brekin.set_local_coord(0, 0, 10, delay=10)
+
     brekin.land_velocity()
     brekin.set_velocity_publish(False)
+
     while not rospy.is_shutdown():
-        #print "Current Lat: ", brekin.current_lat, " Current Lon: ", brekin.current_lon
         print "Landed!"
