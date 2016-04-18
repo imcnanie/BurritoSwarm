5aa542f26751ef2455d6b365c3f50c18c7d2a0f0
Ian McNanie <ian.mcnanie@3drobotics.com>
2016-04-18 02:34:53 -0700

    cleaned up repo, updated burrito swarm interface

--- flight_code/ROS/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ian/BurritoSwarm/flight_code/ROS/src
-# Build directory: /home/ian/BurritoSwarm/flight_code/ROS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
