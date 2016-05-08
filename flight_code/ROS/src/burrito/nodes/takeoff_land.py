77328578ca99d97bebaaa942a482b7c28d8b58ff
Ian McNanie <ian.mcnanie@3drobotics.com>
2016-05-08 12:29:53 -0700

    added takeoff_land

--- flight_code/ROS/src/burrito/nodes/takeoff_land.py
+++ flight_code/ROS/src/burrito/nodes/takeoff_land.py
@@ -10,14 +10,12 @@ time.sleep(0.1)
 pv.start_navigating()
 time.sleep(0.1)
 print "set mode"
-pv.setmode(custom_mode="MANUAL")
 time.sleep(0.5)
 pv.arm()
-out = raw_input()
-pv.setmode(custom_mode="POSCTL")
-time.sleep(1.0)
+#out = raw_input()
+time.sleep(2.0)
 pv.setmode(custom_mode="OFFBOARD")
-#pv.takeoff_velocity(alt=7.0)
+pv.takeoff_velocity(alt=6.0)
 time.sleep(1.0)
 print "out of takeoff"
 utm_coords = utm.from_latlon(37.8733893, -122.3026196)
@@ -27,12 +25,14 @@ pv.update(pv.cur_pos_x, pv.cur_pos_y, 7.0)
 pv.setmode(custom_mode="OFFBOARD")
 #while not pv.reached:
 #       time.sleep(0.025)
-print out
+#print out
 print "at gps, waiting"
 #time.sleep(2.0)
 print "done"
-copters = [pv]
-velocity_goto.SmartRTL(copters)
+#pv.land_velocity()
+pv.setmode(custom_mode="AUTO.LAND")
+#copters = [pv]
+#velocity_goto.SmartRTL(copters)
 #pv.land_velocity()
 print "Landed!"
 
