454c091f6fc984ebd5c59caf11b6ac950d2ce504
JJof3DR <joshua.jacobs@3drobotics.com>
2016-05-08 09:33:53 -0700

    makes ab+pause more fatal

--- flight_code/ROS/src/burrito/nodes/abpause_publisher.py
+++ flight_code/ROS/src/burrito/nodes/abpause_publisher.py
@@ -16,12 +16,21 @@ import os
 import velocity_goto
 import buttonHandler
 
-def postMode(modeID):
+limit = 99
+
+def postMode(modeID, copid):
     if True:
-        set_mode = rospy.ServiceProxy('/mavros/copter1/set_mode', mavros_msgs.srv.SetMode)
+        service_cop = '/mavros/' + copid + '/set_mode'
+        set_mode = rospy.ServiceProxy(service_cop, mavros_msgs.srv.SetMode)
         ret = set_mode(base_mode=0, custom_mode=modeID)
         print "Changing modes to",modeID,": ", ret
         time.sleep(0.1)
+
+def killCop(copid):
+    if True:
+        service_cop = '/mavros/' + copid + '/cmd/arming'
+        arm = rospy.ServiceProxy(service_cop, mavros_msgs.srv.CommandBool)
+        print "Disarming: ", arm(False)
 
 bpub = rospy.Publisher('abpause_buttons', std_msgs.msg.String, queue_size=10)
 rospy.init_node('talker', anonymous=True)
@@ -31,17 +40,24 @@ bh.startListening()
 
 while True:
     if bh.changed():
-        #print bh.buttonState
-
         if bh.buttonState["A"][1] == "Short" and bh.buttonState["B"][1] == "Short" and bh.buttonState["Loiter"][1] == "Short":
             print "ABORT"
             bpub.publish(std_msgs.msg.String("ABORT"))
+            for i in range(limit):
+                copid = 'copter' + str(i+1)
+                killCop(copid)
+
 	if bh.buttonState["Preset1"][1] == "Short":
             print "MANUAL"
-            postMode("MANUAL")
+            for i in range(limit):
+                copid = 'copter' + str(i+1)
+                postMode("MANUAL", copid)
+
 	if bh.buttonState["Fly"][1] == "Short":
             print "POSCTL"
-            postMode("POSCTL")
+            for i in range(limit):
+                copid = 'copter' + str(i+1)
+                postMode("POSCTL", copid)
 
     try:
         pass
